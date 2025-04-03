ec7355f3548146dc7183c22ec9b11e466ea02c99
tinacheng2 <tina0221156@gmail.com>
2025-04-02 21:18:44 -0400

    fix seed import in json add minor changes to router and schema

--- apps/backend/src/routes/serviceRouter.ts
+++ apps/backend/src/routes/serviceRouter.ts
@@ -1,5 +1,4 @@
 import express, { Router, Request, Response } from 'express';
-import { Status, RequestType } from 'database';
 import PrismaClient from '../bin/prisma-client';
 
 const router = Router();
