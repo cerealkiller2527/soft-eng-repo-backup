57b0361cccda63db4b98172e2a4d2d90dc86059a
Matt Nickerson <mattwnickerson39@gmail.com>
2025-04-03 22:48:55 -0400

    Added the ability to service requests with a delete handle and a new button on the request.  Also changed some styling on button components.

--- apps/frontend/src/components/TransportRequestForm.tsx
+++ apps/frontend/src/components/TransportRequestForm.tsx
@@ -23,6 +23,10 @@ export default function TransportRequestForm() {
         setForm({ ...form, [name]: value });
     };
 
+    const handleDelete = (index: number) => {
+        setSubmittedRequests(prevRequests => prevRequests.filter((_, i) => i !== index));
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -81,20 +85,28 @@ export default function TransportRequestForm() {
                     <br/>
                     <label className="p-2 text-gray-800">Additional Notes: </label>
                     <input name="additionalNotes" placeholder="Enter Additional Notes" className="p-8 w-full border border-gray-300 rounded-xl focus:outline-none" onChange={handleChange}></input>
-                    <button type='submit' className="w-full btn btn-primary p-3 px-8 bg-[#012D5A] text-white rounded-xl ">Submit</button>
+                    <button type='submit' className="border border-3 border-[#012D5A] w-full btn btn-primary p-3 px-8 bg-[#012D5A] text-white rounded-xl hover:bg-white hover:text-[#012D5A]">Submit</button>
                 </form>
             {submittedRequests.length > 0 && (
                 <div className="mt-6 p-4 border-t border-gray-300">
                     <h3 className="text-lg font-semibold">Submitted Requests</h3>
                     {submittedRequests.map((request, index) => (
                         <div key={index} className="mt-4 p-2 border-b border-gray-300">
-                            <p><strong>Patient Name:</strong> {request.patientName}</p>
+                            <div>
+                            <p><strong>Patient Name:</strong> {request.patientName}<button
+                                onClick={() => handleDelete(index)}
+                                className="border ml-4 text-red-500 hover:text-red-700 p-2 rounded-l rounded-r justify-end hover:text-white hover:bg-red-500 ">
+                            X
+                            </button></p>
                             <p><strong>Priority:</strong> {request.priority}</p>
                             <p><strong>Transport Type:</strong> {request.transportType}</p>
                             <p><strong>Pickup From:</strong> {request.pickupTransport}</p>
                             <p><strong>Dropoff To:</strong> {request.dropoffTransport}</p>
                             <p><strong>Additional Notes:</strong> {request.additionalNotes || "N/A"}</p>
+                            </div>
+
                         </div>
+
                     ))}
                 </div>
             )}
