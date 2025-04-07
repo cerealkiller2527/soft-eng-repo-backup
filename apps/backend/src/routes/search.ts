244af2b8b8fe0ac4baed448ea0844650951ea72a
mlinman302 <max.inman101@gmail.com>
2025-04-07 09:00:20 -0400

    BFS has expected functionality but roundabout neighbor checking, want to implement cache for more efficient lookup

--- apps/backend/src/routes/search.ts
+++ apps/backend/src/routes/search.ts
@@ -38,16 +38,12 @@ class Node {
             }
         }
 
-        console.log('node id: ', this.id, ' neighbor node ids: ', neighborNodeIds);
-
         // add new ids and current ids (unpacked) into set to remove duplicated then unpack into list
         // old neighbor ids:
-        const existingIds = [];
-        for (let neighbor of this.neighbors) {
-            existingIds.push(neighbor.id);
-        }
-
+        const existingIds = this.neighbors.map((n) => n.id);
         const uniqueNeighborIds = [...new Set([...neighborNodeIds, ...existingIds])];
+
+        console.log('node id: ', this.id, ' neighbor node ids: ', uniqueNeighborIds);
 
         // query for neighbor nodes
         const neighborNodesString = await PrismaClient.node.findMany({
@@ -62,7 +58,6 @@ class Node {
         for (let neighbor of this.neighbors) {
             console.log(neighbor);
         }
-        console.log('exit addNeighbors');
     }
 }
 
@@ -71,7 +66,7 @@ router.get('/', async (req: Request, res: Response) => {
         // start node is A
         let start = new Node(1);
         let end = new Node(4);
-        //
+
         // let ret = await end.addNeighbors();
         // res.json(end.neighbors);
 
@@ -87,31 +82,39 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 async function findBFS(startNode: Node, endNode: Node) {
+    /**
+     * returns a list of Nodes representing the path from startNode to endNode
+     * @param startNode Node the start of the BFS
+     * @param endNode Node the desired location of the BFS
+     * @return path Node[] list of nodes, or empty list if no path found
+     */
     let pathQueue: Node[][] = [[startNode]]; // queue of paths to go through
-    let visited = new Set<Node>(); // set of visited nodes
+    let visited = new Set<Number>(); // set of visited nodes (by ID)
 
     while (pathQueue.length > 0) {
         let currentPath = pathQueue.shift(); // path we are investigating
         let currentNode = currentPath![currentPath!.length - 1]; // not null assertion
         console.log('checking node:' + currentNode.id);
 
-        if (currentNode === endNode) {
+        if (currentNode.id === endNode.id) {
             // if this node is the end node
             console.log('found path');
             return currentPath;
         }
 
-        if (!visited.has(currentNode)) {
+        if (!visited.has(currentNode.id)) {
             // if this node is unvisited
-            visited.add(currentNode); // add it to the visited list
+            visited.add(currentNode.id); // add it to the visited list
 
             // search DB for neighbors
             await currentNode.addNeighbors();
-            console.log('got node (id) ', currentNode.id, "'s neighbors");
+            console.log('got node (id)', currentNode.id, "'s neighbors");
 
             for (let neighbor of currentNode.neighbors) {
                 // unzip current path list into elements, adding neighbor at the end
-                pathQueue.push([...currentPath!, neighbor]);
+                if (!visited.has(neighbor.id)) {
+                    pathQueue.push([...currentPath!, neighbor]);
+                }
             }
             for (let path of pathQueue) {
                 console.log('path: ', path);
