47b45025e099cb32c24a2d714dd06719884368ea
RobertBranchaud <rkbranchaud@wpi.edu>
2025-04-06 22:04:20 -0400

    temp node seeding

--- packages/database/seed.ts
+++ packages/database/seed.ts
@@ -99,6 +99,53 @@ async function main() {
         )
     );
 
+    // seed nodes
+    const nodes = await prisma.node.createManyAndReturn({
+        data: [
+            {name: 'entry', x: 0, y: 0},
+            {name: 'a', x: 0, y: 0},
+            {name: 'lab1', x: 0, y: 0},
+            {name: 'b', x: 0, y: 0},
+            {name: 'c', x: 0, y: 0},
+            {name: 'd', x: 0, y: 0},
+            {name: 'lab2', x: 0, y: 0},
+            {name: 'e', x: 0, y: 0},
+            {name: 'multi-spec clinic', x: 0, y: 0},
+            {name: 'f', x: 0, y: 0},
+            {name: 'radiology', x: 0, y: 0},
+        ]
+    })
+
+    // seed edges with node ids
+    const edges = await prisma.edge.createMany({
+        data: [
+            {fromNodeId: nodes[idFromName("entry")].id, toNodeId: nodes[idFromName("a")].id},
+            {fromNodeId: nodes[idFromName("a")].id, toNodeId: nodes[idFromName("lab1")].id},
+            {fromNodeId: nodes[idFromName("a")].id, toNodeId: nodes[idFromName("b")].id},
+            {fromNodeId: nodes[idFromName("b")].id, toNodeId: nodes[idFromName("c")].id},
+            {fromNodeId: nodes[idFromName("c")].id, toNodeId: nodes[idFromName("d")].id},
+            {fromNodeId: nodes[idFromName("d")].id, toNodeId: nodes[idFromName("lab2")].id},
+            {fromNodeId: nodes[idFromName("d")].id, toNodeId: nodes[idFromName("e")].id},
+            {fromNodeId: nodes[idFromName("e")].id, toNodeId: nodes[idFromName("multi-spec clinic")].id},
+            {fromNodeId: nodes[idFromName("b")].id, toNodeId: nodes[idFromName("f")].id},
+            {fromNodeId: nodes[idFromName("f")].id, toNodeId: nodes[idFromName("radiology")].id}
+        ]
+    })
+
+    /*  idFromName()
+        return the index from nodes[] that corresponds to a given "name"
+        returns -1 if no node is found with that name
+     */
+    function idFromName(name: string): number{
+        for(let i = 0; i < nodes.length; i++){
+            if(nodes[i].name === name){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
     console.log('✅ Seed complete!');
 }
 
