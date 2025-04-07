996b122e20d36483a11f0e076ac59c6bdee05299
SnappyGalaxy511 <brandoncontardi@gmail.com>
2025-04-07 17:30:49 -0400

    Navbar improved

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom';
+import BnWLogo from '/BrighamAndWomensLogo.png';
 
 const Navbar = () => {
     return(
-        <div className={"bg-[#0057B8] py-4"}>
-            <nav id="navbar" className="flex justify-between px-24 space-x-8 mt-2">
-                <Link to="/" className={"text-white text lg font-semibold hover:underline"}>
+        <div className={"bg-gray-100 py-4 w-full border-b border-gray-400 shadow-sm fixed top-0 left-0 z-50"}>
+            <nav id="navbar" className="flex justify-between px-6 space-x-8 mt-2">
+                <img src={BnWLogo} alt="Hospital Logo" className={"flex justify-left  h-10 ml-2"}/>
+                <div className="flex space-x-6 ml-auto">
+                <Link to="/" className={"text-[#0057B8] text-lg font-semibold hover:underline"}>
                     Login
                 </Link>
-                <Link to="/floorplan" className={"text-white text lg font-semibold hover:underline"}>
+                <Link to="/floorplan" className={"text-[#0057B8] text-lg font-semibold hover:underline"}>
                     Floor Plan
                 </Link>
-                <Link to="/ServiceRequest" className={"text-white text lg font-semibold hover:underline"}>
+                <Link to="/ServiceRequest" className={"text-[#0057B8] text-lg font-semibold hover:underline"}>
                     Service Request
                 </Link>
-                <Link to="/Directory" className={"text-white text lg font-semibold hover:underline"}>
+                <Link to="/Directory" className={"text-[#0057B8] text-lg font-semibold hover:underline"}>
                     Directory
                 </Link>
+                </div>
             </nav>
         </div>
     );
