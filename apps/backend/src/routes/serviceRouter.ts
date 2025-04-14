e0c40b2238d08694c6dae744dc7a1d9eadfe7646
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-04-14 19:17:21 -0400

    implemented separate equipmentDelivery router, registered router into serviceRouter.ts

--- apps/backend/src/routes/serviceRouter.ts
+++ apps/backend/src/routes/serviceRouter.ts
@@ -2,7 +2,9 @@ import { initTRPC } from '@trpc/server';
 const t = initTRPC.create();
 
 import { languageRouter } from './languageRouter';
+import { equipmentDeliveryRouter } from './equipmentDeliveryRouter';
 
 export const serviceRouter = t.router({
     languageRouter: languageRouter,
+    equipmentDeliveryRouter: equipmentDeliveryRouter,
 });
