63df36e31eade853d514be5b5f114e010f3657fe
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-04-14 22:37:35 -0400

    implemented separate audiovisual, language, equipmentDelivery, extTransportation routers with getters, add, and update procedures
    registered alls routers into serviceRouter.ts

--- apps/backend/src/routes/serviceRouter.ts
+++ apps/backend/src/routes/serviceRouter.ts
@@ -3,8 +3,14 @@ const t = initTRPC.create();
 
 import { languageRouter } from './languageRouter';
 import { equipmentDeliveryRouter } from './equipmentDeliveryRouter';
+import { securityRouter } from './securityRouter';
+import { externalTransportationRouter } from './externalTransportationRouter';
+import { audiovisualRouter } from './audiovisualRouter';
 
 export const serviceRouter = t.router({
     languageRouter: languageRouter,
     equipmentDeliveryRouter: equipmentDeliveryRouter,
+    securityRouter: securityRouter,
+    externalTransportationRouter: externalTransportationRouter,
+    audiovisualRouter: audiovisualRouter,
 });
