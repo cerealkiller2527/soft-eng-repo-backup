1ba434324704c1f6a42d9e924d71bbbb29025bcc
Madhav Lodha <madhavlodha2503@gmail.com>
2025-04-15 15:15:25 -0400

    fixed issue with pull request, reverted back while adding a the router import

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,17 +1,29 @@
+export default app;
+export type appRouter = typeof appRouter;
+
 import { initTRPC } from '@trpc/server';
 import * as trpcExpress from '@trpc/server/adapters/express';
-import { mapInfoRouter } from './routes/mapInfoRouter';
+import { employeeRouter } from './routes/employeeRouter';
 import { serviceRouter } from './routes/serviceRouter';
-
+import { mapInfoRouter } from './routes/mapInfoRouter';
+import { loginRouter } from './routes/loginRouter.ts';
+import { searchRouter } from './routes/search.ts';
+import { csvRouter } from './routes/csvRouter.ts';
 import express from 'express';
 import logger from 'morgan';
+import { departmentRouter } from './routes/departmentRouter.ts';
 
 // created for each request
 const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({}); // no context
 type Context = Awaited<ReturnType<typeof createContext>>;
 const t = initTRPC.context<Context>().create();
 const appRouter = t.router({
+    employee: employeeRouter,
     service: serviceRouter,
+    login: loginRouter,
+    csv: csvRouter,
+    department: departmentRouter,
+    search: searchRouter,
     mapInfo: mapInfoRouter,
 });
 const app = express();
