34d7ccf075e9e91f2b140ae6feedba93c7a0a9ea
RobertBranchaud <rkbranchaud@wpi.edu>
2025-04-22 19:55:29 -0400

    search is still hard coded, but at east it works now!

--- apps/backend/src/routes/search.ts
+++ apps/backend/src/routes/search.ts
@@ -10,43 +10,8 @@ import PrismaClient from "../bin/prisma-client.ts";
 export const t = initTRPC.create();
 
 export const searchRouter = t.router({
-    getPath: t.procedure
-        .input(
-            z.object({
-                location: z.string(),
-                endDesc: z.string(),
-                driving: z.boolean(),
-            })
-        )
-        .mutation(async ({ input }) => {
-            const { location, endDesc, driving } = input;
 
-            try {
-                const s = new SearchSystem(new BFS());
-                const nodePath = await s.path(location, endDesc, driving);
-
-                // convert each pNode to pNodeDTO
-                const pNodeDTOS: pNodeDTO[] = nodePath.map((node) => ({
-                    id: node.id,
-                    description: node.description,
-                    latitude: node.latitude,
-                    longitude: node.longitude,
-                    floor: node.floor,
-                    neighbors: node.neighbors.map((neighbor) => ({
-                        id: neighbor.id,
-                    })),
-                }));
-                return pNodeDTOS;
-            } catch (err) {
-                console.error('Error fetching nodes:', err);
-                throw new TRPCError({
-                    code: 'INTERNAL_SERVER_ERROR',
-                    message: 'An error occurred while fetching nodes.',
-                });
-            }
-        }),
-
-    path: t.procedure.input(z.object({buildingName: z.string(), endSuite: z.string(), startSuite: z.string(), driving: z.boolean()})).query(async ({ input }) => {
+    getPath: t.procedure.input(z.object({buildingName: z.string(), endSuite: z.string(), startSuite: z.string(), driving: z.boolean()})).query(async ({ input }) => {
         try {
             const endLocation = await PrismaClient.location.findFirst({
                 where: {
