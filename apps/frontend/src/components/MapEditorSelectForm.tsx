0a21d40f94caa2dbc95bd855db512e84ba1775a2
Matt Nickerson <mattwnickerson39@gmail.com>
2025-04-22 19:43:18 -0400

    map form basic NEEDS BACKEND INTEGRATION

--- apps/frontend/src/components/MapEditorSelectForm.tsx
+++ apps/frontend/src/components/MapEditorSelectForm.tsx
@@ -21,7 +21,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
-
+import MapForm from "./MapForm";
 export const formSchema = z.object({
     building: z.string(),
     floor: z.string(),
@@ -64,6 +64,10 @@ export default function MapEditorSelectForm({ onSubmit }: { onSubmit: (values: z
     };
 
     return (
+        <div>
+            <div>
+            <MapForm />
+            </div>
         <Form {...form}>
             <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
                 <FormField
@@ -121,5 +125,6 @@ export default function MapEditorSelectForm({ onSubmit }: { onSubmit: (values: z
                 <Button type="submit">Submit</Button>
             </form>
         </Form>
+        </div>
     );
 }
