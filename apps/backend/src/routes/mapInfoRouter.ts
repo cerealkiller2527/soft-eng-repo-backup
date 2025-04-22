19c10f814e887a7ed4743216113be418cccb3791
RobertBranchaud <rkbranchaud@wpi.edu>
2025-04-22 18:41:14 -0400

    now returns suites instead of nodes

--- apps/backend/src/routes/mapInfoRouter.ts
+++ apps/backend/src/routes/mapInfoRouter.ts
@@ -1,6 +1,7 @@
 import {initTRPC} from "@trpc/server";
 import PrismaClient from "../bin/prisma-client.ts";
 import {z} from "zod";
+import prismaClient from "../bin/prisma-client.ts";
 
 export const t = initTRPC.create();
 export const mapInfoRouter = t.router({
@@ -18,18 +19,18 @@ export const mapInfoRouter = t.router({
                     throw new Error(`Building with name '${input.buildingName}' not found`);
                 }
 
-                // get nodes in building by locations
-                return await PrismaClient.node.findMany({
+                const locations = await PrismaClient.location.findMany({
                     where: {
-                        Location: {
-                            some: {
-                                buildingId: building.id,
-                            },
-                        },
-                    },
-                });
-
-
+                        buildingId: building.id,
+                    }
+                })
+                return [
+                    ...new Set(
+                        locations
+                            .map(location => location.suite)
+                            .filter((suite): suite is string => suite !== null && suite !== undefined)
+                    ),
+                ];
             } catch (error) {
                 console.error(`Error fetching nodes for building '${input.buildingName}':`, error);
                 throw new Error('Failed to fetch building nodes');
