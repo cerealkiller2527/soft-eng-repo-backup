7b665abba7ebcffbd2dc2b0cf56542a1fff28211
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-04-25 15:47:01 -0400

    fixed t.router issues - moved t and context export up before importing routers in app.ts

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,33 +1,37 @@
 import { initTRPC } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
-import { clerkClient } from '@clerk/clerk-sdk-node';
+import { clerkClient } from "@clerk/clerk-sdk-node";
+import express from "express";
+import logger from "morgan";
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
+export const t = initTRPC.context<Context>().create();
+
 import { employeeRouter } from "./routes/employeeRouter";
 import { serviceRouter } from "./routes/serviceRouter";
 import { loginRouter } from "./routes/loginRouter.ts";
 import { searchRouter } from "./routes/search.ts";
 import { csvRouter } from "./routes/csvRouter.ts";
-import express from "express";
-import logger from "morgan";
 import { directoriesRouter } from "./routes/directoriesRouter.ts";
 import { mapEditorRouter } from "./routes/mapEditorRouter.ts";
 import { mapInfoRouter } from "./routes/mapInfoRouter.ts";
 
 // created for each request
-const createContext = async ({ req, res }: trpcExpress.CreateExpressContextOptions) => {
-    try {
-        const session = await clerkClient.sessions.getSession(req);
+const createContext = async ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  try {
+    const session = await clerkClient.sessions.getSession(req);
 
-        return {
-            userId: session ? session.userId : null,
-        };
-    } catch (error) {
-        console.error("Error fetching Clerk session:", error);
-        return { userId: null }; //
-    }
+    return {
+      userId: session ? session.userId : null,
+    };
+  } catch (error) {
+    console.error("Error fetching Clerk session:", error);
+    return { userId: null }; //
+  }
 };
-
-export type Context = Awaited<ReturnType<typeof createContext>>;
-export const t = initTRPC.context<Context>().create();
 
 const appRouter = t.router({
   employee: employeeRouter,
