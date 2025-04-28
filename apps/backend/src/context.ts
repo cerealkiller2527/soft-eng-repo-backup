c8a8da8c0687b1a865ddf9925144e536ad09a7bb
tinacheng2 <tina0221156@gmail.com>
2025-04-27 23:42:50 -0400

    clerk protected the routers in the backend and routes in the frontend

---
+++ apps/backend/src/context.ts
@@ -0,0 +1,22 @@
+import { clerkClient } from "@clerk/clerk-sdk-node";
+import { getAuth } from "@clerk/express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+
+export const createContext = async ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return { userId: null, role: null };
+  }
+
+  const user = await clerkClient.users.getUser(userId);
+  const role = user.publicMetadata?.role as string | null;
+  const username = user.username;
+
+  return { userId, role, username };
+};
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
