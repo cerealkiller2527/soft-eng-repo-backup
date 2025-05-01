aa0ac92e09f45062688041cab9a5c3fb266e4937
mlinman302 <max.inman101@gmail.com>
2025-04-30 22:33:11 -0400

    basic trpc implementation

--- apps/frontend/src/routes/dbEditor.tsx
+++ apps/frontend/src/routes/dbEditor.tsx
@@ -1,13 +1,28 @@
 import Layout from "@/components/Layout.tsx";
 import React from "react";
 
+import { useTRPC } from '../database/trpc.ts';
+import {useMutation, useQuery} from '@tanstack/react-query';
+
 
 export default function DbEditor() {
+
+    const trpc = useTRPC();
+
+    const { data: employees, isLoading, error } = useQuery({
+        queryKey: ['employees'],
+        queryFn: () => trpc.data.getEmployees.query()
+    });
+
+    if (isLoading) return <p>... Loading ...</p>
+
+    if (error) return <p> Error! </p>
+
     return (
         <Layout>
             <div className={"container max-w-full pt-8"}>
-                <div className={"flex flex-wrap justify-center p-8"}>
-                    <h1>Hello World</h1>
+                <div className={"flex flex-wrap justify-center items-center p-8"}>
+                    <h1>{JSON.stringify(data, null, 2)}</h1>
                 </div>
             </div>
         </Layout>
