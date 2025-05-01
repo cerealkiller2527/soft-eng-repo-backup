6a166b0c20d18bdd914410423b6c3c141a4704fe
RobertBranchaud <rkbranchaud@wpi.edu>
2025-04-30 22:13:31 -0400

    initial setup for front end testing page for database editor

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -13,6 +13,7 @@ import { csvRouter } from "./routes/csvRouter.ts";
 import { directoriesRouter } from "./routes/directoriesRouter.ts";
 import { mapEditorRouter } from "./routes/mapEditorRouter.ts";
 import { mapInfoRouter } from "./routes/mapInfoRouter.ts";
+import {dbImportRouter} from "./routes/dbImportRouter.ts";
 
 const appRouter = t.router({
   employee: employeeRouter,
@@ -22,6 +23,7 @@ const appRouter = t.router({
   search: searchRouter,
   mapEditor: mapEditorRouter,
   mapInfoRouter: mapInfoRouter,
+  dpImport: dbImportRouter
 });
 
 const app = express();
---
+++ apps/frontend/src/routes/dbEditor.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout.tsx";
+import React from "react";
+
+
+export default function DbEditor() {
+    return (
+        <Layout>
+            <div className={"container max-w-full pt-8"}>
+                <div className={"flex flex-wrap justify-center p-8"}>
+                    <h1>Hello World</h1>
+                </div>
+            </div>
+        </Layout>
+    )
+}
